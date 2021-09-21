1b6bd1aab0b7e38980db8081d4e6b8fde5ca049a
Rasmusbothomsen <rasmus0408@gmai.com>
2021-09-21 21:29:46 +0200

    Test af metoden til at kaste med terning

---
+++ SourceCode/src/DiceTest.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class DiceTest {
+    private static void main(String[]args){
+        int dice2;
+        int dice1;
+        int count=0;
+        while(count<1000) {
+            Random dice = new Random();
+            dice1 = dice.nextInt(6) + 1;
+            dice2 = dice.nextInt(6) + 1;
+            System.out.println(dice1 + "\t" + dice2 + "\n");
+            count++;
+        }
+
+    }
+}
