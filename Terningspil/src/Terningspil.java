b5273e175b6cbf7aa069e626cfcc62c159ffdd57
Oskar <oskarlolklarsen@gmail.com>
2021-09-27 16:39:29 +0200

    Tilføjet is40 methode, som tjekker hvorvidt en spiller har 40 points

--- Terningspil/src/Terningspil.java
+++ Terningspil/src/Terningspil.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 public class Terningspil {
 
     public static void main(String[] args) {
@@ -11,11 +9,11 @@ public class Terningspil {
         Player player1 = new Player(scan.nextLine());
         System.out.print("Spiller 2 indtast dit navn: ");
         Player player2 = new Player(scan.nextLine());
-//regler
+        Dice dice = new Dice();
+        dice.getDice();
         System.out.println("Man vinder ved at opnå 40 point, ved at slå to ens får vedkommende en ekstra tur.");
         System.out.println("Ved slag af to en'ere mister vedkommende alle deres point - dog gælder ovenstående stadig.");
-
-
+        is40(player1);
 
 
 
@@ -23,6 +21,10 @@ public class Terningspil {
     private static void startgame(){
         System.out.println("Velkommen til terningespillet");
 
+    }
+    private static boolean is40(Player a){
+        if (a.getPoint()<40) return false;
+        else return true;
     }
 
 
