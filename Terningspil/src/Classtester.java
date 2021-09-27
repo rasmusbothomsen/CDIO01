5ec3db1f78ffb7e2d79dbdeeee2153390aa5167e
Rasmus Thomsen <rasmus0408@gmail.com>
2021-09-27 16:29:21 +0200

    Dice class virker, men kan getDice for at få et array med sine slag

--- Terningspil/src/Classtester.java
+++ Terningspil/src/Classtester.java
@@ -1,6 +1,7 @@
 public class Classtester {
     public static void  main(String[]args){
         Dice dice = new Dice();
-
+        int[] slag = dice.getDice();
+        System.out.println(slag[0]+" "+slag[1]);
     }
 }
--- Terningspil/src/Dice.java
+++ Terningspil/src/Dice.java
@@ -2,12 +2,11 @@ import java.util.Random;
 
 public class Dice {
     private Random dice = new Random();
-    private int dice1;
-    private int dice2;
+
 
     public int[] getDice() {
-        this.dice1 = dice.nextInt(6) + 1;
-        this.dice2 = dice.nextInt(6) + 1;
-        return new int[]{dice1, dice2};
+        int dice1 = dice.nextInt(6)+1;
+        int dice2 = dice.nextInt(6)+1;
+        return new int[]{dice1,dice2};
     }
 }
