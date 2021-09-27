6feb93eb7d0bdf87d69d373d8b220e79ea2813d0
Oskar <oskarlolklarsen@gmail.com>
2021-09-27 16:03:52 +0200

    Starten på terningspil, der er oprettet main i terningspil

--- Terningspil/src/Terningspil.java
+++ Terningspil/src/Terningspil.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
+
+
 public class Terningspil {
+
+    public static void main(String[] args) {
+
+        System.out.println("Velkommen til terningespillet");
+
+        int player1 = 1;
+        int player2 = 0;
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
