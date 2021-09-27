df3d567ece403de74274fe2638b2506c5fa63f10
Christoffer Fink <christo.fink.1@gmail.com>
2021-09-27 16:38:27 +0200

    regler

--- Terningspil/src/Terningspil.java
+++ Terningspil/src/Terningspil.java
@@ -12,10 +12,8 @@ public class Terningspil {
         System.out.print("Spiller 2 indtast dit navn: ");
         Player player2 = new Player(scan.nextLine());
 //regler
-        System.out.println("Man vinder ved at opnå 40 point, ved at slå to ens får vedkommende en ekstra tur.");
+        System.out.println("Man vinder ved at opnå 40 point og efterfølgende slå to ens, ved at slå to ens får vedkommende en ekstra tur.");
         System.out.println("Ved slag af to en'ere mister vedkommende alle deres point - dog gælder ovenstående stadig.");
-
-
 
 
 
