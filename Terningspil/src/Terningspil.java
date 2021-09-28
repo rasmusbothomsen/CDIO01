db3404dde112ed4cbb99e3734df2e75e33a2649b
MartinFilrup <martinfilrup@gmail.com>
2021-09-28 20:14:39 +0200

    Oprettet metode is40 som om en spiller er over 40

--- Terningspil/src/Terningspil.java
+++ Terningspil/src/Terningspil.java
@@ -19,6 +19,8 @@ public class Terningspil {
 
 
 
+
+
     }
     private static void startgame(){
         System.out.println("Velkommen til terningespillet");
@@ -26,13 +28,23 @@ public class Terningspil {
     }
     private static boolean is40(Player a){
         if (a.getPoint()<40) return false;
-        else return true;
+        else true;
     }
+
+    //Demo flow start
+
+    private static boolean toEndGame(Player a){
+        if ;
+        else return true;
+
+    //Demo flow slut
+
     private static void regler() {
         System.out.println("Man vinder ved at opnå 40 point og efterfølgende slå to ens.");
         System.out.println("Når man slår to ens får vedkommende en ekstra tur");
         System.out.println("Ved slag af to en'ere mister vedkommende alle deres point - dog gælder ovenstående stadig.");
     }
+
 
 
 
