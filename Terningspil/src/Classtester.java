4d870a40b6ffd9f7733844f36fad0bd4fa358d1c
Rasmus Thomsen <rasmus0408@gmail.com>
2021-09-30 10:43:13 +0200

    Tilføjet timer, og testet tid det tager at spille

--- Terningspil/src/Classtester.java
+++ Terningspil/src/Classtester.java
@@ -1,5 +1,13 @@
+
+import java.time.Duration;
+import java.time.Instant;
+
 public class Classtester {
-    public static void  main(String[]args){
+    public static void  main(String[]args) throws InterruptedException {
+        Instant start = Instant.now();
+        Thread.sleep(500);
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end));
         Dice dice = new Dice();
         int[] slag = dice.getDice();
         System.out.println(slag[0]+" "+slag[1]);
